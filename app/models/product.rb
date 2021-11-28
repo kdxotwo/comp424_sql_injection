0ca99e2654eb99d51550c21c5cfbb9548dcb03b4
kadiatou <kdxotwo@gmail.com>
2021-11-27 18:00:58 -0800

    added the secure way to the search queries, registration and login are already implemented

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,11 @@ class Product < ApplicationRecord
     has_many :orders
 
     def self.search(search_queries)
+        #insecure way
         Product.where("name LIKE '%#{search_queries}%'")
+
+        #secure way, it will escape and sanitize the queries.
+        # it'll throw away an error
+        # Product.where("name LIKE '%?%'", search_queries)
     end
 end
